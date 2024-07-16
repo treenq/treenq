c46eab472c5107fab826c52cfd307f4554a1eb88
jamshad.adil <jamshad.adil@gmail.com>
2024-07-16 18:18:10 +0500

    added mutex

--- src/repo/secret/infisical.go
+++ src/repo/secret/infisical.go
@@ -1,30 +1,33 @@
 package secret
 
 import (
+	"sync"
+
 	infisical "github.com/infisical/go-sdk"
 )
 
 type Secret struct {
 	client infisical.InfisicalClientInterface
+	mu     sync.Mutex
 }
 
-func NewSecretsManager(clientId string, secret string) (*Secret, error) {
+func NewSecret(secret *Secret) (*Secret, error) {
+	secret.mu.Lock()
 	client := infisical.NewInfisicalClient(infisical.Config{
 		SiteUrl: "https://app.infisical.com",
 	})
 	// use INFISICAL_UNIVERSAL_AUTH_CLIENT_ID, INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET env
-	_, err := client.Auth().UniversalAuthLogin(clientId, secret)
+	_, err := client.Auth().UniversalAuthLogin("", "")
 	if err != nil {
 		return nil, err
 	}
-
-	return &Secret{
-		client: client,
-	}, nil
+	secret.client = client
+	defer secret.mu.Unlock()
+	return secret, nil
 }
 
-func (sm *Secret) GetSecret(secretKey string, env string, projectId string) (infisical.Secret, error) {
-	return sm.client.Secrets().Retrieve(
+func (se *Secret) GetSecret(secretKey string, env string, projectId string) (infisical.Secret, error) {
+	return se.client.Secrets().Retrieve(
 		infisical.RetrieveSecretOptions{
 			SecretKey:   secretKey,
 			Environment: env,
