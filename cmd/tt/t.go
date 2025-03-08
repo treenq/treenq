1c7e13e94c3411dbafa914818fa2fdf82b67baf4
Denis <candyboobers@gmail.com>
2025-03-08 20:58:05 +0100

    Try running buildah in a container

---
+++ cmd/tt/t.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/containers/buildah/define"
+	"github.com/containers/buildah/imagebuildah"
+	"github.com/containers/storage"
+	"github.com/containers/storage/pkg/reexec"
+)
+
+func main() {
+	reexec.Init()
+	buildStoreOptions, err := storage.DefaultStoreOptions()
+	ifErr(err)
+	buildStore, err := storage.GetStore(buildStoreOptions)
+	ifErr(err)
+
+	id, ref, err := imagebuildah.BuildDockerfiles(context.Background(), buildStore, define.BuildOptions{
+		Registry:     "jopa",
+		Output:       "jopa2",
+		Out:          os.Stdout,
+		Err:          os.Stderr,
+		ReportWriter: os.Stdout,
+		IgnoreFile:   "./.dockerignore",
+	}, "./Dockerfile")
+	ifErr(err)
+	fmt.Println(id)
+	fmt.Println(ref)
+}
+
+func ifErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
