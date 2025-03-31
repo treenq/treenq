da27817abe9af4931b9bc62eba6a6c579b54e3c7
Denis Dvornikov <candyboobers@gmail.com>
2025-03-31 23:52:19 +0200

    Play with fetching a redirect page on sign in (#52)

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly APP_API_HOST: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: 'APP',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
