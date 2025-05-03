1f1812a544fdf553d7d718232e9df173e6989a30
Denis Dvornikov <candyboobers@gmail.com>
2025-05-03 17:31:40 +0000

    Add solid devtools

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
 /* @refresh reload */
+import 'solid-devtools'
 import { render } from 'solid-js/web'
 import App from './App.tsx'
 import './main.css'
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import devtools from 'solid-devtools/vite'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [
+    devtools({
+      autoname: true,
+      locator: true,
+    }),
+    solid(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
