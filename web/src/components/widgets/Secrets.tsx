de5daedfaf766880e6cff83d774621fd0b7ce9d0
8180e <emirceylan31@hotmail.com>
2025-05-27 20:47:55 +0200

    Add inputs to secret rows

--- web/src/components/widgets/Secrets.tsx
+++ web/src/components/widgets/Secrets.tsx
@@ -7,15 +7,37 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/Table'
-import { createSignal, For } from 'solid-js'
+import { TextField, TextFieldInput } from '@/components/ui/TextField'
+import { createSignal, For, Setter } from 'solid-js'
 
-type Secret = {
-  name: string
-  value: string
+type Secret = { name: string; value: string }
+
+type SecretRowProps = Secret & { index: number; setSecrets: Setter<Secret[]> }
+
+const SecretRow = ({ name, value, index, setSecrets }: SecretRowProps) => {
+  const [readOnly, setReadOnly] = createSignal(true)
+
+  const updateSecret = (value: string) =>
+    setSecrets((secrets) => secrets.map((s, i) => (i === index ? { ...s, value } : s)))
+
+  return (
+    <TableRow>
+      <TableCell>{name}</TableCell>
+      <TableCell>
+        <TextField onChange={updateSecret} readOnly={readOnly()}>
+          <TextFieldInput value={value} type="password" />
+        </TextField>
+      </TableCell>
+      <TableCell>
+        <Button onClick={() => setReadOnly(false)}>Edit</Button>
+        <Button>Delete</Button>
+      </TableCell>
+    </TableRow>
+  )
 }
 
 const Secrets = () => {
-  const [secrets] = createSignal<Secret[]>([
+  const [secrets, setSecrets] = createSignal<Secret[]>([
     { name: 'API_KEY', value: '123abc' },
     { name: 'DB_PASSWORD', value: 'password123' },
     { name: 'SECRET_TOKEN', value: 'token456' },
@@ -32,16 +54,7 @@ const Secrets = () => {
       </TableHeader>
       <TableBody>
         <For each={secrets()}>
-          {({ name, value }) => (
-            <TableRow>
-              <TableCell>{name}</TableCell>
-              <TableCell>{value}</TableCell>
-              <TableCell>
-                <Button>Edit</Button>
-                <Button>Delete</Button>
-              </TableCell>
-            </TableRow>
-          )}
+          {(secret, index) => <SecretRow {...secret} index={index()} setSecrets={setSecrets} />}
         </For>
       </TableBody>
     </Table>
