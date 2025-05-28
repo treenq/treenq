9a0be2c96d7dfe34bcc80e815ababf6d2a6b5ab1
8180e <emirceylan31@hotmail.com>
2025-05-28 20:32:47 +0200

    Add secrets tab (#75)
    * Fix auth redirect on page refresh
    
    * Add secrets for the repo page
    
    * Add table display for secrets
    
    * Add inputs to secret rows
    
    * Fix readOnly signal update bug
    
    * Add secret adding row and secret name changing to the secrets table
    
    * Refactor secrets table

--- web/src/components/pages/RepoPage.tsx
+++ web/src/components/pages/RepoPage.tsx
@@ -1,6 +1,8 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/Tabs'
+import Deploy from '@/components/widgets/Deploy'
+import Secrets from '@/components/widgets/Secrets'
 import { useParams } from '@solidjs/router'
 import { createEffect } from 'solid-js'
-import Deploy from '../widgets/Deploy'
 
 export default function RepoPage() {
   const params = useParams()
@@ -14,9 +16,20 @@ export default function RepoPage() {
   // show list of connected repositories
 
   return (
-    <main class="bg-background flex min-h-screen flex-col items-center justify-center py-12">
+    <main class="bg-background flex min-h-screen flex-col items-center py-12">
       <div class="flex w-full max-w-3xl flex-col items-center gap-10">
-        <Deploy repoID={params.id} />
+        <Tabs defaultValue="deployments" class="w-full">
+          <TabsList class="grid w-full grid-cols-2">
+            <TabsTrigger value="deployments">Deployments</TabsTrigger>
+            <TabsTrigger value="secrets">Secrets</TabsTrigger>
+          </TabsList>
+          <TabsContent value="deployments">
+            <Deploy repoID={params.id} />
+          </TabsContent>
+          <TabsContent value="secrets">
+            <Secrets />
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   )
---
+++ web/src/components/widgets/Secrets.tsx
@@ -0,0 +1,131 @@
+import { Button } from '@/components/ui/Button'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/Table'
+import { TextField, TextFieldInput } from '@/components/ui/TextField'
+import { Accessor, createSignal, Index, JSX, Setter, Show } from 'solid-js'
+
+type Secret = { name: string; value: string }
+
+type SecretRowProps = { secret: Accessor<Secret>; index: number; setSecrets: Setter<Secret[]> }
+
+type SecretTableRowProps = {
+  isEditing: Accessor<boolean>
+  inputs: Accessor<Secret>
+  setInputs: Setter<Secret>
+  secret?: Accessor<Secret>
+  children: JSX.Element
+}
+
+const SecretTableRow = ({
+  isEditing,
+  inputs,
+  setInputs,
+  secret,
+  children,
+}: SecretTableRowProps) => {
+  const [toggleVisible, setToggleVisible] = createSignal(false)
+
+  return (
+    <TableRow>
+      <TableCell>
+        <Show when={isEditing()} fallback={secret && secret().name}>
+          <TextField
+            value={inputs().name}
+            onChange={(name) => setInputs((inputs) => ({ ...inputs, name }))}
+          >
+            <TextFieldInput placeholder="SECRET_NAME" />
+          </TextField>
+        </Show>
+      </TableCell>
+      <TableCell class="flex">
+        <TextField
+          value={inputs().value}
+          onChange={(value) => setInputs((inputs) => ({ ...inputs, value }))}
+          readOnly={!isEditing()}
+          class="flex-1"
+        >
+          <TextFieldInput placeholder="Secret value" type={toggleVisible() ? 'text' : 'password'} />
+        </TextField>
+        <Button onClick={() => setToggleVisible(!toggleVisible())}>Toggle</Button>
+      </TableCell>
+      <TableCell>{children}</TableCell>
+    </TableRow>
+  )
+}
+
+const SecretRow = ({ secret, index, setSecrets }: SecretRowProps) => {
+  const [inputs, setInputs] = createSignal<Secret>(secret())
+  const [isEditing, setIsEditing] = createSignal(false)
+
+  const updateSecret = () => {
+    setSecrets((secrets) => secrets.map((s, i) => (i === index ? inputs() : s)))
+    setIsEditing(false)
+  }
+
+  const deleteSecret = () => setSecrets((secrets) => secrets.filter((_, i) => i !== index))
+
+  return (
+    <SecretTableRow {...{ isEditing, inputs, setInputs, secret }}>
+      <div class="flex">
+        <Show
+          when={isEditing()}
+          fallback={<Button onClick={() => setIsEditing(true)}>Edit</Button>}
+        >
+          <Button onClick={updateSecret}>Save</Button>
+        </Show>
+        <Button onClick={deleteSecret}>Delete</Button>
+      </div>
+    </SecretTableRow>
+  )
+}
+
+const AddSecretRow = ({ setSecrets }: { setSecrets: Setter<Secret[]> }) => {
+  const [inputs, setInputs] = createSignal<Secret>({ name: '', value: '' })
+
+  const addSecret = () => {
+    setSecrets((secrets) => [...secrets, inputs()])
+    setInputs({ name: '', value: '' })
+  }
+
+  return (
+    <SecretTableRow isEditing={() => true} inputs={inputs} setInputs={setInputs}>
+      <Button disabled={!inputs().name || !inputs().value} onClick={addSecret}>
+        Add
+      </Button>
+    </SecretTableRow>
+  )
+}
+
+const Secrets = () => {
+  const [secrets, setSecrets] = createSignal<Secret[]>([
+    { name: 'API_KEY', value: '123abc' },
+    { name: 'DB_PASSWORD', value: 'password123' },
+    { name: 'SECRET_TOKEN', value: 'token456' },
+  ])
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-sm">Name</TableHead>
+          <TableHead class="w-3xl">Value</TableHead>
+          <TableHead class="w-3xs">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <Index each={secrets()}>
+          {(secret, index) => <SecretRow {...{ secret, index, setSecrets }} />}
+        </Index>
+        <AddSecretRow setSecrets={setSecrets} />
+      </TableBody>
+    </Table>
+  )
+}
+
+export default Secrets
