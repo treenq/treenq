c0d37d6b43c5628d4f978e7c1902bd31cfc7f408
8180e <emirceylan31@hotmail.com>
2025-05-28 18:47:07 +0200

    Add secret adding row and secret name changing to the secrets table

--- web/src/components/widgets/Secrets.tsx
+++ web/src/components/widgets/Secrets.tsx
@@ -8,29 +8,95 @@ import {
   TableRow,
 } from '@/components/ui/Table'
 import { TextField, TextFieldInput } from '@/components/ui/TextField'
-import { Accessor, createSignal, Index, Setter } from 'solid-js'
+import { Accessor, createSignal, Index, Setter, Show } from 'solid-js'
 
 type Secret = { name: string; value: string }
 
 type SecretRowProps = { secret: Accessor<Secret>; index: number; setSecrets: Setter<Secret[]> }
 
 const SecretRow = ({ secret, index, setSecrets }: SecretRowProps) => {
-  const [readOnly, setReadOnly] = createSignal(true)
+  const [inputs, setInputs] = createSignal<Secret>(secret())
+  const [isEditing, setIsEditing] = createSignal(false)
+  const [toggleVisible, setToggleVisible] = createSignal(false)
 
-  const updateSecret = (value: string) =>
-    setSecrets((secrets) => secrets.map((s, i) => (i === index ? { ...s, value } : s)))
+  const updateSecret = () => {
+    setSecrets((secrets) => secrets.map((s, i) => (i === index ? inputs() : s)))
+    setIsEditing(false)
+  }
+
+  const deleteSecret = () => setSecrets((secrets) => secrets.filter((_, i) => i !== index))
 
   return (
     <TableRow>
-      <TableCell>{secret().name}</TableCell>
       <TableCell>
-        <TextField onChange={updateSecret} readOnly={readOnly()}>
-          <TextFieldInput value={secret().value} type="password" />
+        <Show when={isEditing()} fallback={secret().name}>
+          <TextField
+            value={inputs().name}
+            onChange={(name) => setInputs((inputs) => ({ ...inputs, name }))}
+          >
+            <TextFieldInput />
+          </TextField>
+        </Show>
+      </TableCell>
+      <TableCell class="flex">
+        <TextField
+          value={inputs().value}
+          onChange={(value) => setInputs((inputs) => ({ ...inputs, value }))}
+          readOnly={!isEditing()}
+          class="flex-1"
+        >
+          <TextFieldInput type={toggleVisible() ? 'text' : 'password'} />
         </TextField>
+        <Button onClick={() => setToggleVisible(!toggleVisible())}>Toggle</Button>
       </TableCell>
       <TableCell>
-        <Button onClick={() => setReadOnly(false)}>Edit</Button>
-        <Button>Delete</Button>
+        <div class="flex">
+          <Show
+            when={isEditing()}
+            fallback={<Button onClick={() => setIsEditing(true)}>Edit</Button>}
+          >
+            <Button onClick={updateSecret}>Save</Button>
+          </Show>
+          <Button onClick={deleteSecret}>Delete</Button>
+        </div>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+const AddSecretRow = ({ setSecrets }: { setSecrets: Setter<Secret[]> }) => {
+  const [toggleVisible, setToggleVisible] = createSignal(false)
+  const [inputs, setInputs] = createSignal<Secret>({ name: '', value: '' })
+
+  const addSecret = () => {
+    setSecrets((secrets) => [...secrets, inputs()])
+    setInputs({ name: '', value: '' })
+  }
+
+  return (
+    <TableRow>
+      <TableCell>
+        <TextField
+          value={inputs().name}
+          onChange={(name) => setInputs((inputs) => ({ ...inputs, name }))}
+        >
+          <TextFieldInput placeholder="SECRET_NAME" />
+        </TextField>
+      </TableCell>
+      <TableCell class="flex">
+        <TextField
+          value={inputs().value}
+          onChange={(value) => setInputs((inputs) => ({ ...inputs, value }))}
+          class="flex-1"
+        >
+          <TextFieldInput placeholder="Secret value" type={toggleVisible() ? 'text' : 'password'} />
+        </TextField>
+        <Button onClick={() => setToggleVisible(!toggleVisible())}>Toggle</Button>
+      </TableCell>
+      <TableCell>
+        <Button disabled={!inputs().name || !inputs().value} onClick={addSecret}>
+          Add
+        </Button>
       </TableCell>
     </TableRow>
   )
@@ -56,6 +122,7 @@ const Secrets = () => {
         <Index each={secrets()}>
           {(secret, index) => <SecretRow {...{ secret, index, setSecrets }} />}
         </Index>
+        <AddSecretRow setSecrets={setSecrets} />
       </TableBody>
     </Table>
   )
