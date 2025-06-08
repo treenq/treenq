bf1261985e1f90200a9fff153e8551ad67d3c240
Denis <666trall666@gmail.com>
2025-06-08 19:52:09 +0300

    feat add sprite

--- web/src/components/widgets/ConsoleDeploy.tsx
+++ web/src/components/widgets/ConsoleDeploy.tsx
@@ -15,6 +15,8 @@ import { deployStore } from '@/store/deployStore'
 import { VariantProps } from 'class-variance-authority'
 import { createEffect, createSignal } from 'solid-js'
 
+import { Icon } from '../ui/Icon'
+
 const STATUS_DEPLOYMENT: Record<DeploymentStatus, BadgeVariant> = {
   run: 'default',
   failed: 'error',
@@ -72,6 +74,7 @@ export default function ConsoleDeploy() {
       </div>
 
       <Console classNames="mb-3" logs={logs()} />
+      <Icon name="iconUploadFile" />
     </Card>
   )
 }
