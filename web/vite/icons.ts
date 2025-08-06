f8dbbfc0a54bcb46ecfacd717647cdbffa74a2c4
Denis <candyboobers@gmail.com>
2025-08-06 21:58:05 +0200

    Fix icons sprite generation

--- web/vite/icons.ts
+++ web/vite/icons.ts
@@ -3,33 +3,37 @@ import path from 'path'
 
 export default function IconSpritePlugin() {
   async function generateIconSprite() {
-    // Read the SVG files in the static/icons folder
-    const iconsDir = path.join(process.cwd(), 'public/static', 'icons')
-    const files = await fs.readdir(iconsDir)
-    let symbols = ''
-    const iconNames: string[] = []
+    try {
+      // Read the SVG files in the static/icons folder
+      const iconsDir = path.join(process.cwd(), 'public/static', 'icons')
+      const files = await fs.readdir(iconsDir)
+      let symbols = ''
+      const iconNames: string[] = []
 
-    // Build up the SVG sprite from the SVG files
-    for (const file of files) {
-      if (!file.endsWith('.svg')) continue
-      let svgContent = await fs.readFile(path.join(iconsDir, file), 'utf8')
-      const id = file.replace('.svg', '')
-      iconNames.push(id)
-      svgContent = svgContent
-        .replace(/id="[^"]+"/, '') // Remove any existing id
-        .replace('<svg', `<symbol id="${id}"`) // Change <svg> to <symbol>
-        .replace('</svg>', '</symbol>')
-      symbols += svgContent + '\n'
+      // Build up the SVG sprite from the SVG files
+      for (const file of files) {
+        if (!file.endsWith('.svg')) continue
+        let svgContent = await fs.readFile(path.join(iconsDir, file), 'utf8')
+        const id = file.replace('.svg', '')
+        iconNames.push(id)
+        svgContent = svgContent
+          .replace(/id="[^"]+"/g, '') // Remove any existing id
+          .replace('<svg', `<symbol id="${id}"`) // Change <svg> to <symbol>
+          .replace('</svg>', '</symbol>')
+        symbols += svgContent + '\n'
+      }
+
+      // Write the SVG sprite to a file in the static folder
+      const sprite = `<svg width="0" height="0" style="display: none">\n\n${symbols}</svg>`
+      await fs.writeFile(path.join(process.cwd(), 'public/static', 'icon-sprite.svg'), sprite)
+
+      // generate ts file for icon names
+      const dtsContent = `export type IconName = ${iconNames.map((name) => `'${name}'`).join(' | ')};\n`
+      const dtsFilePath = path.join(process.cwd(), 'src/components/icons', 'icon-names.ts')
+      await fs.writeFile(dtsFilePath, dtsContent)
+    } catch (error) {
+      console.error('Error generating icon sprite:', error)
     }
-
-    // Write the SVG sprite to a file in the static folder
-    const sprite = `<svg width="0" height="0" style="display: none">\n\n${symbols}</svg>`
-    await fs.writeFile(path.join(process.cwd(), 'public/static', 'icon-sprite.svg'), sprite)
-
-    // generate ts file for icon names
-    const dtsContent = `export type IconName = ${iconNames.map((name) => `'${name}'`).join(' | ')};\n`
-    const dtsFilePath = path.join(process.cwd(), 'src/components/icons', 'icon-names.ts')
-    await fs.writeFile(dtsFilePath, dtsContent)
   }
 
   return {
@@ -40,9 +44,12 @@ export default function IconSpritePlugin() {
     },
     configureServer(server: any) {
       // Regenerate during development whenever an icon is added
-      server.watcher.add(path.join(process.cwd(), 'static', 'icons', '*.svg'))
+      const iconsPath = path.join(process.cwd(), 'public/static/icons')
+      server.watcher.add(iconsPath)
       server.watcher.on('change', async (changedPath: any) => {
-        if (changedPath.endsWith('.svg')) return generateIconSprite()
+        if (changedPath.includes('public/static/icons') && changedPath.endsWith('.svg')) {
+          await generateIconSprite()
+        }
       })
     },
   }
